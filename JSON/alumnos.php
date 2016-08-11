61581f9752405e857a8629f3be05c3ef7f0a3acc
Adolfo Raya <araya0912@hotmail.com>
2016-08-10 19:45:34 -0500

    creación de la carpeta JSON

---
+++ JSON/alumnos.php
@@ -0,0 +1,9 @@
+<?php 
+	$resultado = '[';
+
+	$resultado .= '{"nombre":"Bruno","ap1":"Diaz","ap2":"Lopez"},';
+	$resultado .= '{"nombre":"Adriana","ap1":"Aguilar","ap2":"Hernandez"}';
+	
+	$resultado .= ']';
+	echo $resultado;
+ ?>
---
+++ JSON/script.js
@@ -0,0 +1,32 @@
+function getAlumnos(){
+	var ruta = "alumnos.php";
+
+	solicitarDatos(ruta,recibirDatos);
+}
+
+function solicitarDatos(ruta, funcion){
+	var xhttp = new XMLHttpRequest();
+  	xhttp.onreadystatechange = function() {
+	    if (xhttp.readyState == 4 && xhttp.status == 200) {
+	       funcion(xhttp.responseText);
+	    }
+    };
+  
+  	xhttp.open("POST", ruta, true);
+    xhttp.send();
+}
+
+function recibirDatos(respuestaServer){
+	var respuesta = JSON.parse(respuestaServer);
+	var salida = '';
+
+	for(i = 0; i < respuesta.length; i++){
+		salida += "<tr>";
+		salida += "<td>"+respuesta[i].nombre+"</td>"
+		salida += "<td>"+respuesta[i].ap1+"</td>"
+		salida += "<td>"+respuesta[i].ap2+"</td>"
+		salida += "</tr>";
+	}
+
+	document.getElementById('cuerpo').innerHTML = salida;
+}
